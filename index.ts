778e6b1b2a5f686150f6d8406420af06b73de74a
Satyam Anand <satyam.anand@bolt.earth>
2022-10-25 12:46:09 +0530

    direction panel

--- index.ts
+++ index.ts
@@ -4,12 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-const BOLT_URL = "https://bolt.revos.in"
-const APP_TOKEN = import.meta.env.VITE_APP_TOKEN
-const AUTH_TOKEN = import.meta.env.VITE_AUTH_TOKEN
+const BOLT_URL = "https://bolt.revos.in";
+const APP_TOKEN = import.meta.env.VITE_APP_TOKEN;
+const AUTH_TOKEN = import.meta.env.VITE_AUTH_TOKEN;
 
 function initMap(): void {
-
   const map = new google.maps.Map(
     document.getElementById("map") as HTMLElement,
     {
@@ -20,7 +19,6 @@ function initMap(): void {
   );
 
   new AutocompleteDirectionsHandler(map);
-
 }
 
 class AutocompleteDirectionsHandler {
@@ -76,7 +74,6 @@ class AutocompleteDirectionsHandler {
     this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(
       destinationInput
     );
-
   }
 
   setupPlaceChangedListener(
@@ -94,17 +91,17 @@ class AutocompleteDirectionsHandler {
       }
 
       for (let charger of this.chargerMarkers) {
-        charger.setMap(null)
+        charger.setMap(null);
       }
       for (let waypoint of this.waypointMarkers) {
-        waypoint.setMap(null)
+        waypoint.setMap(null);
       }
       this.waypoints = [];
 
       let request = {
         placeId: place.place_id,
-        fields: ["geometry"]
-      }
+        fields: ["geometry"],
+      };
 
       let service = new google.maps.places.PlacesService(this.map);
       const callback = (
@@ -113,23 +110,23 @@ class AutocompleteDirectionsHandler {
       ) => {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
           if (mode === "ORIG") {
-            this.startMarker?.setMap(null)
+            this.startMarker?.setMap(null);
             this.startMarker = new google.maps.Marker({
               position: place?.geometry?.location,
               label: "A",
-              map: this.map
-            })
+              map: this.map,
+            });
           } else {
-            this.endMarker?.setMap(null)
+            this.endMarker?.setMap(null);
             this.endMarker = new google.maps.Marker({
               position: place?.geometry?.location,
               label: "B",
-              map: this.map
-            })
+              map: this.map,
+            });
           }
         }
-      }
-      service.getDetails(request, callback)
+      };
+      service.getDetails(request, callback);
 
       if (mode === "ORIG") {
         this.originPlaceId = place.place_id;
@@ -156,16 +153,56 @@ class AutocompleteDirectionsHandler {
 
         waypoints: this.waypoints,
         optimizeWaypoints: true,
-        provideRouteAlternatives: false,
+        provideRouteAlternatives: true,
       },
       (response, status) => {
         if (status === "OK" && response) {
-          me.directionsRenderer.setDirections(response);
+          // me.directionsRenderer.setDirections(response);
 
-          if (shouldGetChargers) {
-            this.getChargers(response.routes[0].bounds)
+          /**
+           * This is to display in the direction panel
+           */
+          const alternateSummaryPanel = document.getElementById(
+            "directions-panel"
+          ) as HTMLElement;
+
+          alternateSummaryPanel.innerHTML = "";
+          for (let j = 0; j < response.routes.length; j++) {
+            let route = response.routes[j];
+
+            // let newDirectionsRendere = new google.maps.DirectionsRenderer({
+            //   suppressMarkers: true,
+            //   draggable: true,
+            //   hideRouteList: false,
+            //   routeIndex: j,
+            // });
+
+            me.directionsRenderer.setOptions({
+              draggable: true,
+              hideRouteList: false,
+              routeIndex: j
+            })
+            
+            me.directionsRenderer.setDirections(response);
+
+            // For each route, display summary information.
+            for (let i = 0; i < route.legs.length; i++) {
+              const routeSegment = j + 1;
+              alternateSummaryPanel.innerHTML +=
+                "<b>Route Segment: " + routeSegment + "</b><br>";
+              alternateSummaryPanel.innerHTML +=
+                route.legs[i].start_address + " to ";
+              alternateSummaryPanel.innerHTML +=
+                route.legs[i].end_address + "<br>";
+              alternateSummaryPanel.innerHTML +=
+                route.legs[i].distance!.text + "<br>";
+              alternateSummaryPanel.innerHTML +=
+                route.legs[i].duration!.text + "<br><br>";
+            }
+            if (shouldGetChargers) {
+              this.getChargers(response.routes[j].bounds);
+            }
           }
-
         } else {
           window.alert("Directions request failed due to " + status);
         }
@@ -173,46 +210,46 @@ class AutocompleteDirectionsHandler {
     );
   }
 
-  getChargers(
-    bounds: google.maps.LatLngBounds,
-  ) {
+  getChargers(bounds: google.maps.LatLngBounds) {
+    const lat_top = bounds.getNorthEast().lat();
+    const lat_bottom = bounds.getSouthWest().lat();
+    const lng_left = bounds.getSouthWest().lng();
+    const lng_right = bounds.getNorthEast().lng();
 
-    const lat_top = bounds.getNorthEast().lat()
-    const lat_bottom = bounds.getSouthWest().lat()
-    const lng_left = bounds.getSouthWest().lng()
-    const lng_right = bounds.getNorthEast().lng()
-
-    fetch(`${BOLT_URL}/charger/getAvailable?lat_top=${lat_top}&lat_bottom=${lat_bottom}&lng_left=${lng_left}&lng_right=${lng_right}`, {
-      headers: {
-        token: APP_TOKEN,
-        Authorization: `Bearer ${AUTH_TOKEN}`,
+    fetch(
+      `${BOLT_URL}/charger/getAvailable?lat_top=${lat_top}&lat_bottom=${lat_bottom}&lng_left=${lng_left}&lng_right=${lng_right}`,
+      {
+        headers: {
+          token: APP_TOKEN,
+          Authorization: `Bearer ${AUTH_TOKEN}`,
+        },
       }
-    })
+    )
       .then((res) => res.json())
       .then((res) => {
-        if (!res.data.length) window.alert("No chargers found")
-        else res.data.forEach((el: any, i: number) => {
-          const position = new google.maps.LatLng({
-            lat: el.station.location.latitude,
-            lng: el.station.location.longitude,
-          })
-          this.createChargerMarker(position, el.charger.chargerId)
-        })
+        if (!res.data.length) window.alert("No chargers found");
+        else
+          res.data.forEach((el: any, i: number) => {
+            const position = new google.maps.LatLng({
+              lat: el.station.location.latitude,
+              lng: el.station.location.longitude,
+            });
+            this.createChargerMarker(position, el.charger.chargerId);
+          });
       })
       .catch((err) => {
-        console.error(err)
-      })
-
+        console.error(err);
+      });
   }
 
   createChargerMarker(position: google.maps.LatLng, chargerId: string) {
     let chargerMarker = new google.maps.Marker({
       position,
       icon: {
-        url: "./images/charger-available.svg"
+        url: "./images/charger-available.svg",
       },
       map: this.map,
-    })
+    });
 
     const infoWindow = new google.maps.InfoWindow({
       content: `<b>${chargerId}</b><br/>Click to add waypoint`,
@@ -226,30 +263,29 @@ class AutocompleteDirectionsHandler {
       infoWindow.close();
     });
     chargerMarker.addListener("click", () => {
-
       if (this.waypoints.length === 25) {
-        window.alert("Max limit of 25 waypoints reached")
+        window.alert("Max limit of 25 waypoints reached");
       } else {
-        let markerIndex = this.chargerMarkers.findIndex(el => el.getPosition()?.toString() === position.toString())
-        chargerMarker.setMap(null)
-        this.chargerMarkers.splice(markerIndex, 1)
+        let markerIndex = this.chargerMarkers.findIndex(
+          (el) => el.getPosition()?.toString() === position.toString()
+        );
+        chargerMarker.setMap(null);
+        this.chargerMarkers.splice(markerIndex, 1);
 
-        this.createWaypointMarker(position, chargerId)
-        this.route(false)
-        infoWindow.close()
+        this.createWaypointMarker(position, chargerId);
+        this.route(false);
+        infoWindow.close();
       }
-
     });
 
-    this.chargerMarkers.push(chargerMarker)
-
+    this.chargerMarkers.push(chargerMarker);
   }
 
   createWaypointMarker(position: google.maps.LatLng, chargerId: string) {
     let waypointMarker = new google.maps.Marker({
       position,
       map: this.map,
-    })
+    });
 
     const infoWindow = new google.maps.InfoWindow({
       content: `<b>${chargerId}</b><br/>Click to remove waypoint`,
@@ -263,23 +299,28 @@ class AutocompleteDirectionsHandler {
       infoWindow.close();
     });
     waypointMarker.addListener("click", () => {
-      let markerIndex = this.waypointMarkers.findIndex(el => el.getPosition()?.toString() === position.toString())
-      if (markerIndex !== -1) this.waypointMarkers.splice(markerIndex, 1)
+      let markerIndex = this.waypointMarkers.findIndex(
+        (el) => el.getPosition()?.toString() === position.toString()
+      );
+      if (markerIndex !== -1) this.waypointMarkers.splice(markerIndex, 1);
 
-      let waypointIndex = this.waypoints.findIndex(el => el.location?.toString() === position.toString())
-      if (waypointIndex !== -1) this.waypoints.splice(waypointIndex, 1)
+      let waypointIndex = this.waypoints.findIndex(
+        (el) => el.location?.toString() === position.toString()
+      );
+      if (waypointIndex !== -1) this.waypoints.splice(waypointIndex, 1);
 
-      waypointMarker.setMap(null)
+      waypointMarker.setMap(null);
 
-      this.createChargerMarker(position, chargerId)
-      this.route(false)
-      infoWindow.close()
-
+      this.createChargerMarker(position, chargerId);
+      this.route(false);
+      infoWindow.close();
     });
 
-    this.waypoints.push({ location: position, stopover: true } as google.maps.DirectionsWaypoint);
-    this.waypointMarkers.push(waypointMarker)
-
+    this.waypoints.push({
+      location: position,
+      stopover: true,
+    } as google.maps.DirectionsWaypoint);
+    this.waypointMarkers.push(waypointMarker);
   }
 }
 
@@ -289,4 +330,4 @@ declare global {
   }
 }
 window.initMap = initMap;
-export { };
+export {};
